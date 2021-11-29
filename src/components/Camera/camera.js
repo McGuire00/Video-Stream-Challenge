02a8af8593f80526f612553c6013537b20c6fd42
Deron McGuire <Deronmcguire23@gmail.com>
2021-11-29 17:05:16 -0600

    Added buttons

--- src/components/Camera/camera.js
+++ src/components/Camera/camera.js
@@ -1,31 +1,46 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Webcam from "webcam-easy";
 
 import "./camera.css";
 
 function Camera() {
-  // https://stackoverflow.com/questions/46142783/dom-isnt-loaded-yet-when-react-tries-to-registers-component-which-runs-in-windo
-
-  document.addEventListener("DOMContentLoaded", function (event) {
-    const webcamElement = document.getElementById("webcam");
-    const canvasElement = document.getElementById("canvas");
-    const webcam = new Webcam(webcamElement, "user", canvasElement);
-    webcam
-      .start()
-      .then((result) => {
-        console.log("webcam started");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  });
-
-  // useEffect(() =>{
+  // document.addEventListener("DOMContentLoaded", function (event) {
   //   const webcamElement = document.getElementById("webcam");
   //   const canvasElement = document.getElementById("canvas");
   //   const webcam = new Webcam(webcamElement, "user", canvasElement);
   //   webcam.start()
-  // })
+  // });
+
+  // https://stackoverflow.com/questions/46142783/dom-isnt-loaded-yet-when-react-tries-to-registers-component-which-runs-in-windo
+  // document.addEventListener("DOMContentLoaded", function (event) {
+  //   const webcamElement = document.getElementById("webcam");
+  //   const canvasElement = document.getElementById("canvas");
+  //   const webcam = new Webcam(webcamElement, "user", canvasElement);
+  //   webcam
+  //     .start()
+  //     .then((result) => {
+  //       console.log("webcam started");
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // });
+
+  function handleStart() {
+    const webcamElement = document.getElementById("webcam");
+    const canvasElement = document.getElementById("canvas");
+    const webcam = new Webcam(webcamElement, "user", canvasElement);
+
+    webcam.stream();
+  }
+
+  function handleStop() {
+    const webcamElement = document.getElementById("webcam");
+    const canvasElement = document.getElementById("canvas");
+    const webcam = new Webcam(webcamElement, "user", canvasElement);
+
+    webcam.stop();
+  }
 
   return (
     <div className="video-container">
@@ -40,8 +55,13 @@ function Camera() {
         <canvas id="canvas"></canvas>
       </div>
       <div className="btns">
-        <button className="btn-1">Stop Recording</button>
-        <button className="btn-2">Submit Video</button>
+        <button className="btn-1" onClick={handleStart}>
+          Start Recording
+        </button>
+        <button className="btn-2" onClick={handleStop}>
+          Stop Recording
+        </button>
+        <button className="btn-3">Submit Video</button>
       </div>
     </div>
   );
