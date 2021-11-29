021f019c75d97838ef0b7f5e9b62cee096d827ce
Deron McGuire <Deronmcguire23@gmail.com>
2021-11-28 19:10:02 -0600

    Added buttons

--- src/components/Camera/camera.js
+++ src/components/Camera/camera.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Webcam from "webcam-easy";
 
 import "./camera.css";
 
 function Camera() {
-  const webcamElement = document.getElementById("webcam");
-  const canvasElement = document.getElementById("canvas");
-  const webcam = new Webcam(webcamElement, "user", canvasElement);
+  // https://stackoverflow.com/questions/46142783/dom-isnt-loaded-yet-when-react-tries-to-registers-component-which-runs-in-windo
 
-  webcam
-    .start()
-    .then((result) => {
-      console.log("webcam started");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  document.addEventListener("DOMContentLoaded", function (event) {
+    const webcamElement = document.getElementById("webcam");
+    const canvasElement = document.getElementById("canvas");
+    const webcam = new Webcam(webcamElement, "user", canvasElement);
+    webcam
+      .start()
+      .then((result) => {
+        console.log("webcam started");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
+  // useEffect(() =>{
+  //   const webcamElement = document.getElementById("webcam");
+  //   const canvasElement = document.getElementById("canvas");
+  //   const webcam = new Webcam(webcamElement, "user", canvasElement);
+  //   webcam.start()
+  // })
 
   return (
-    <div>
-      {/* <video id="webcam" autoplay playsinline width="640" height="480"></video>
-      <canvas id="canvas" class="d-none"></canvas> */}
+    <div className="video-container">
+      <div className="video-feed">
+        <video
+          id="webcam"
+          autoplay
+          playsinline
+          width="640"
+          height="480"
+        ></video>
+        <canvas id="canvas"></canvas>
+      </div>
+      <div className="btns">
+        <button className="btn-1">Stop Recording</button>
+        <button className="btn-2">Submit Video</button>
+      </div>
     </div>
   );
 }
